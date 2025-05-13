aefc182b52ca49d4e7eb564f3e8d1acd8ac31505
Alexkamil77 <alexanderjocana@gmail.com>
2025-05-13 13:41:17 -0300

    Update generate-hashes.js

--- generate-hashes.js
+++ generate-hashes.js
@@ -1,21 +1,32 @@
 const bcrypt = require('bcrypt');
+const fs = require('fs');
 
-const users = [
-    { username: "Alexander", password: "senha123" },
-    { username: "Lucas", password: "senha123" },
-    { username: "Alberto", password: "senha123" },
-    { username: "Fernanda", password: "senha123" },
-    { username: "Thais", password: "senha123" },
-    { username: "Ivani", password: "senha123" },
-    { username: "Vacina", password: "senha123" },
-    { username: "Triagem", password: "senha123" }
-];
+const users = {
+    medicos: [
+        { username: "Alexander", password: "senha123" },
+        { username: "Lucas", password: "senha123" },
+        { username: "Alberto", password: "senha123" }
+    ],
+    enfermeiras: [
+        { username: "Fernanda", password: "senha123" },
+        { username: "Thais", password: "senha123" },
+        { username: "Ivani", password: "senha123" }
+    ],
+    outros: [
+        { username: "Vacina", password: "senha123" },
+        { username: "Triagem", password: "senha123" }
+    ]
+};
 
 async function generateHashes() {
-    for (const user of users) {
-        const hashedPassword = await bcrypt.hash(user.password, 10);
-        console.log(`Username: ${user.username}, Hashed Password: ${hashedPassword}`);
+    for (const group in users) {
+        for (const user of users[group]) {
+            user.password = await bcrypt.hash(user.password, 10);
+        }
     }
+
+    fs.writeFileSync('users.json', JSON.stringify(users, null, 2));
+    console.log('Hashes gerados e salvos no arquivo users.json!');
 }
 
 generateHashes();
